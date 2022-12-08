f8e52ce10c6446818d643092eda13e4b81c7abe0
genii grace <jeyeom10@naver.com>
2022-12-08 17:47:44 +0900

    배열, 배열의 요소를 변수명으로 받음

---
+++ Day02_Day45/15_2.1.5-3.js
@@ -0,0 +1,9 @@
+//배열
+//배열안에 들어갈 수 있는 요소에 제한이 없다
+
+var array=['nodejs',{}, 10, true];
+var node = array[0];
+var obj=array[1];
+var bool=array[3];
+
+console.log(node, obj, bool);
---
+++ Day02_Day45/16_2.1.5-4.js
@@ -0,0 +1,9 @@
+//배열
+//배열의 요소를 변수 명으로 받을 수 있다.
+
+const array=['nodejs',{},10, true];
+
+const [node,obj,,bool]=array;
+
+console.log(array);
+console.log(obj);
