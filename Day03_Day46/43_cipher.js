6e3ecff134fd9a6d6f0eddd882dfde64a97e627f
genii grace <jeyeom10@naver.com>
2022-12-09 15:58:57 +0900

    데이터 암호화 : 양방향 암호화

--- Day03_Day46/43_cipher.js
+++ Day03_Day46/43_cipher.js
@@ -13,6 +13,6 @@ result += cipher.final('base64');
 console.log('암호화 : ', result);
 
 const decipher = crypto.createDecipheriv(algorithm, key, iv);
-let result2 = decipher.update(resutl, 'base64', 'utf8');
+let result2 = decipher.update(result, 'base64', 'utf8');
 result2+=decipher.final('utf8');
 console.log('복호화 : ',result2);
